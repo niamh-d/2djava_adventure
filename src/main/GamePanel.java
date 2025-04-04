8be33f695da18e28699724a4bfbfe8d9cd7aa128
Niamh <niamh.linguist@gmail.com>
2025-04-04 13:34:11 +0300

    game panel opens

---
+++ src/main/GamePanel.java
@@ -0,0 +1,24 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 px
+    final int screenHeight = tileSize * maxScreenRow; // 576 px
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -10,6 +10,11 @@ public class Main {
         window.setResizable(false);
         window.setTitle("2D Adventure");
 
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
         window.setLocationRelativeTo(null);
         window.setVisible(true);
     }
