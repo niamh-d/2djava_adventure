9e932d3f838967034575aafa00ec92445442892d
Niamh <niamh.linguist@gmail.com>
2025-04-04 13:48:52 +0300

    white square drawn to screen

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,7 +3,7 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     // SCREEN SETTINGS
     final int originalTileSize = 16; // 16x16 tile
@@ -15,10 +15,48 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; // 768 px
     final int screenHeight = tileSize * maxScreenRow; // 576 px
 
+    Thread gameThread;
+
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        while(gameThread != null) {
+
+            System.out.println("The game loop is running.");
+
+            // 1 UPDATE: update info such as character pos
+            update();
+            // 2 DRAW: draw screen with updated info
+            repaint();
+        }
+    }
+
+    public void update() {
+
+    }
+
+    // called by repaint()
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D)g;
+
+        g2d.setColor(Color.white);
+
+        g2d.fillRect(100, 100, tileSize, tileSize);
+
+        g2d.dispose();
     }
 }
